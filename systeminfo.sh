16a473b7fe4d6c9a8bb2d3c157d7ce89b31214e1
imriishav <rishavrajput472@gmail.com>
2026-06-13 11:35:27 +0000

    added

---
+++ systeminfo.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+	echo "=====SYSTEM INFORMATION===="
+
+	echo "hostname:"
+	hostname
+
+	echo "System user:"
+	whoami
+
+	echo "Date:"
+	date
+
+	echo "Current Directory:"
+	pwd
+
+	echo "uptime:"
+	uptime
+	
+	echo "kernal version:"
+	uname -r
+
